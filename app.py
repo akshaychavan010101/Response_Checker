5ee61cb8db4d0a2d20bcbe8a189ef8ac9e4152a0
akshaychavan010101 <akshaychavan010101@gmail.com>
2023-07-22 20:14:26 +0530

    frontend separated

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, json
 import openai
 import os
 from models.main_checker import chatGPT
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv()
 
@@ -10,23 +11,25 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/")
 def index():
-    return render_template("home.html")
+    return 'Hello World'
 
 
 @app.route("/generate", methods=["POST"])
 def generate():
     try:
-        data = request.json
+        data = request.get_json()
         q = data["question"]
         a = data["answer"]
         response = chatGPT(q, a)
-        return render_template("home.html", response=response)
-    except:
-        return render_template("home.html", response="Something went wrong!")
+        return json.dumps({"data": response, "ok": True})
+    except Exception as e:
+        print(e)
+        return json.dumps({"message": "Something went wrong", "ok": False})
 
 
 if __name__ == "__main__":
